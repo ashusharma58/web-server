30ee9db481129d562557c4146cca898cbc614674
Ashish Sharma <ashuysharma@gmail.com>
2019-10-30 15:17:07 +0530

    Removed unused code

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This msg is from js folder');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
